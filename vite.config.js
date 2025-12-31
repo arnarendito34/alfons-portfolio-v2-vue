147a4aa42637441aaac37eed671c338e44de7d27
arnarendito34 <renditobloggerspot@gmail.com>
2025-12-31 14:33:03 +0700

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/portfolio-alfons/',
-    plugins: [vue()],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
-            },
-        },
-    }
+  base: '/',
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+          "import"
+        ],
+      },
+    },
+  },
 })
